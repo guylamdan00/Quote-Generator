996b44281ab10f0e448132b0cbd148a7fedcb246
guylamdan00 <guylamdan00@gmail.com>
2021-07-26 19:31:28 +0300

    JavaScript - Get Quotes from api function

---
+++ script.js
@@ -0,0 +1,23 @@
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    // Pick a Random Quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiURL = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote()
+    } catch(error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
