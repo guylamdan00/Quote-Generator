6cd5900a17d7c6f0ef6a34a7ed4d3fddd07466f8
guylamdan00 <guylamdan00@gmail.com>
2021-07-28 22:43:01 +0300

    Loading animation

--- script.js
+++ script.js
@@ -3,14 +3,28 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a Random Quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    // Check if Author field is blank and replace it with 'Unknown';
+    // Check if Author field is blank and replace it with 'Unknown'
     if (!quote.author) {
         authorText.textContent = 'Unknown';
     } else{
@@ -22,17 +36,20 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+    // Set Quote, Hide Loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiURL = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
-        newQuote()
-    } catch(error) {
+        newQuote();
+    } catch (error) {
         // Catch Error Here
     }
 }
